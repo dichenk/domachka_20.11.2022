acc2076217fb8c88b090ec56d9fed738e53d9c2d
OLEG DIACHENKO <dchenk@gmail.com>
2022-11-21 19:03:08 +0300

    program is working, that's fine

--- solve.py
+++ solve.py
@@ -1,1 +1,16 @@
-.
+def solve(n, repeats):
+    a = 0
+    b = 1
+    repeats = int(repeats)
+    while repeats > 0:
+        a += int(repeats) * int(n) * b
+        b *= 10
+        repeats -= 1
+
+    return a
+
+n = input('please input a nubmer: ')
+repeats = input('please input amount of repeats: ')
+print()
+print('result: ', solve(n, repeats))
+
